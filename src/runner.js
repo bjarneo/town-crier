fbc90b7087f508e5196292d7c5cbd92a92143af8
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2016-03-26 13:11:51 +0100

    refactor

---
+++ src/runner.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const spread = require('array-spread');
+const fetch = require('./fetch');
+const transformData = require('./transformation/transform-data');
+const sortByDate = require('./sort/sort-by-date');
+const write = require('./write');
+const config = require('../config');
+
+function handleData(providers) {
+    providers = providers.map(transformData);
+
+    // Same as ...providers (ES6)
+    providers = spread(providers);
+
+    providers = providers.sort(sortByDate);
+
+    providers.forEach(write);
+
+    setTimeout(runner, config.interval);
+}
+
+function runner() {
+    fetch(config.sources)
+        .then(handleData)
+        .catch(err => setTimeout(() => { throw new Error(err); })) // get full stack hack
+        .catch(console.error);
+}
+
+module.exports = runner;
+
