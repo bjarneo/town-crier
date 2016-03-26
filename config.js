520215bca76372656c161c63b40b2e4439bd52cd
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2016-03-26 13:26:08 +0100

    add dateformat

--- config.js
+++ config.js
@@ -16,10 +16,14 @@ argv.run();
 console.log(argv);
 */
 module.exports = {
-    interval: 60 * 1000, // 60 seconds
+    interval: 60 * 1000, // 60 seconds,
+    dateFormat: 'dd-mm-yyyy', // mm-dd-yyyy
     sources: [
         'http://www.dagbladet.no/rss/nyheter/',
         'http://www.aftenposten.no/rss/',
-        'http://www.vg.no/rss/feed/?categories=1068&keywords=&limit=10&format=rss'
+        'http://www.vg.no/rss/feed/?categories=1068&keywords=&limit=10&format=rss',
+        'http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml',
+        'http://feeds.reuters.com/reuters/topNews',
+        'http://www.nasa.gov/rss/image_of_the_day.rss'
     ]
 }
--- src/transformation/transform-date.js
+++ src/transformation/transform-date.js
@@ -1,11 +1,24 @@
 'use strict';
 
 const zeroPad = require('../zero-pad');
+const config = require('../../config');
 
 module.exports = function transformDate(date) {
     date = new Date(date);
 
-    return zeroPad(date.getHours() + 1) + ':' +
-        zeroPad(date.getMinutes()) + ':' +
-        zeroPad(date.getSeconds());
+    var dateFormat;
+    if (config.dateFormat === 'mm-dd-yyyy') {
+        dateFormat = zeroPad(date.getMonth()) +
+            '-' + zeroPad(date.getDate()) +
+            '-' + date.getFullYear();
+    } else {
+        dateFormat = zeroPad(date.getDate()) +
+            '-' + zeroPad(date.getMonth()) +
+            '-' + date.getFullYear();
+    }
+
+    return dateFormat +
+        ' ' + zeroPad(date.getHours() + 1) +
+        ':' + zeroPad(date.getMinutes()) +
+        ':' + zeroPad(date.getSeconds());
 };
