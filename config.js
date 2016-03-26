2c926b82e06b0ef4b74a5b5c9b7a2d4fbe9a267c
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2016-03-26 17:26:01 +0100

    update sources

--- config.js
+++ config.js
@@ -28,10 +28,9 @@ const config = {
     dateFormat: (argv.dateFormat || argv.dF) || 'dd-mm-yyyy', // mm-dd-yyyy
     sources: [
         'http://www.vg.no/rss/feed/?categories=1068&keywords=&limit=10&format=rss',
-        'http://www.aftenposten.no/rss/',
         'http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml',
-        'http://www.dagbladet.no/rss/nyheter/',
-        'http://feeds.reuters.com/reuters/topNews'
+        'http://feeds.reuters.com/reuters/topNews',
+        'http://www.theguardian.com/world/rss'
     ]
 };
 
