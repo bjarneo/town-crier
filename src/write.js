f8ab670cdfd954e724c89cb27a9f77d1e9e6a427
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2016-03-27 10:29:41 +0200

    update title formatting

--- src/write.js
+++ src/write.js
@@ -5,7 +5,7 @@ const transformDate = require('./transformation/transform-date');
 
 module.exports = function write(data) {
     console.log(
-        chalk.underline('[%s] [%s]') + '\n%s \n%s\n%s\n\n',
+        chalk.underline('[%s] [%s]') + '\n> %s \n%s\n%s\n\n',
         chalk.dim(transformDate(data.date)),
         chalk.dim(data.provider),
         data.title,
