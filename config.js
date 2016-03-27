350975549ad0c7c79ab8588cf1a6d1f5d1a54abe
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2016-03-27 10:25:49 +0200

    remove keyword

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const config = {
     interval: (argv.interval || 60) * 1000, // 60 seconds,
     dateFormat: argv.dateFormat || 'dd-mm-yyyy', // mm-dd-yyyy | yyyy-mm-dd
     sources: [
-        'http://www.vg.no/rss/feed/?categories=1068&keywords=&limit=10&format=rss',
+        'http://www.vg.no/rss/feed/?categories=1068&limit=10&format=rss',
         'http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml',
         'http://feeds.reuters.com/reuters/topNews',
         'http://www.theguardian.com/world/rss',
