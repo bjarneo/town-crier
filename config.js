55a47ade4f4882a2dc8d887fe5433b7fc74d8166
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2016-03-27 14:19:48 +0200

    add new config file

--- config.js
+++ config.js
@@ -1,31 +1,6 @@
-'use strict';
-
-const meow = require('meow');
-const argv = require('minimist')(process.argv.slice(2));
-
-if (argv.help) {
-    meow(`
-        Usage
-          $ town-crier <input>
-
-        Options
-          --help           Help
-          --interval       Interval in seconds
-          --dateFormat     How you'd like your date format
-          --sources        Comma separated list of rss sources
-
-        Examples
-          $ town-crier --interval=60
-          $ town-crier --dateFormat=mm-dd-yyyy
-          $ town-crier --sources=http://www.vg.no/rss/feed/,http://www.aftenposten.no/rss/
-    `);
-
-    process.exit(-1);
-}
-
-const config = {
-    interval: (argv.interval || 60) * 1000, // 60 seconds,
-    dateFormat: argv.dateFormat || 'dd-mm-yyyy', // mm-dd-yyyy | yyyy-mm-dd
+module.exports = {
+    interval: 60 * 1000, // 60 seconds,
+    dateFormat: 'dd-mm-yyyy', // mm-dd-yyyy | yyyy-mm-dd
     sources: [
         'http://www.vg.no/rss/feed/?categories=1068&limit=10&format=rss',
         'http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml',
@@ -34,10 +9,3 @@ const config = {
         'https://news.ycombinator.com/rss'
     ]
 };
-
-// TODO: create a RSS XML feed validator
-if (argv.sources) {
-    Array.prototype.push.apply(config.sources, argv.sources.split(','));
-}
-
-module.exports = config;
