1d944b2fa08b60d76438da564c52abccb14ef4ab
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2016-03-27 14:35:29 +0200

    step one interaction

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
 'use strict';
 
-require('./src/runner')();
+const fs = require('fs');
+const inquirer = require('inquirer');
+const config = require('./src/configurator');
+const run = require('./src/runner');
+
+const feeds = [];
+const sources = config.initialSources || config.sources;
+
+sources.forEach((source) => {
+    feeds.push({
+        name: source
+    });
+});
+
+function updateConfig(items) {
+    console.log(items);
+
+    if (!config.initialSources) {
+        config.initialSources = config.sources;
+    }
+
+    config.sources = items.sources;
+
+    var data = 'module.exports = ' + JSON.stringify(config);
+
+    fs.writeFile('config.js', data, 'utf-8', err => console.error);
+
+    run();
+}
+
+// TODO: All interaction should be in the configurator
+inquirer.prompt([
+    {
+        type: 'checkbox',
+        message: 'Select RSS feed(s)',
+        name: 'sources',
+        choices: feeds,
+        validate: sources => {
+            if (!sources.length) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+], updateConfig);
