e786e9481ff808327d4837cd3b2f7ad34af1bcf4
bjarne-overli <bjarne.overli@vg.no>
2016-04-01 21:18:08 +0200

    use got instead of request

--- src/fetch.js
+++ src/fetch.js
@@ -1,37 +1,11 @@
 'use strict';
 
-const request = require('request');
-const parse = require('xml2js').parseString;
+const got = require('got');
 
-function promisify(source) {
-    function resolver(resolve, reject) {
-        request({
-            url: source,
-            timeout: 15000
-        }, (err, res, body) => {
-            if (err || res.statusCode !== 200) {
-                reject(err);
-            }
-
-            parse(body, (err, res) => {
-                if (err) {
-                    reject(err);
-                }
-
-                 resolve(res);
-            });
-        }).end();
-    }
-
-    return new Promise(resolver);
-}
-
-function fetch(sources) {
+module.exports = function fetch(sources) {
     var promises = [];
 
-    sources.forEach(source => promises.push(promisify(source)));
+    sources.forEach(source => promises.push(got(source)));
 
     return Promise.all(promises);
-}
-
-module.exports = fetch;
+};
