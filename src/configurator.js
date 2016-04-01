0c04cb179d7340da86353cf4d52375f03f6eec7c
bjarne-overli <bjarne.overli@vg.no>
2016-04-01 21:28:52 +0200

    leverage minimist from meow instead of minimist directly

--- src/configurator.js
+++ src/configurator.js
@@ -1,50 +1,49 @@
 'use strict';
 
 const meow = require('meow');
-const argv = require('minimist')(process.argv.slice(2));
 
 function configurator(config, cb) {
     const inquiries = require('./interaction/inquiries')(config);
 
-    if (!argv.help && (!config.build || argv.reset)) {
+    const cli = meow(`
+        Usage
+          $ town-crier <input>
+
+        Options
+          --help           Help
+          --interval       Interval in seconds
+          --dateFormat     How you'd like your date format
+          --sources        Comma separated list of rss sources you want to append current list
+          --reset          Reset the config file
+
+        Examples
+          $ town-crier --interval=60
+          $ town-crier --dateFormat=mm-dd-yyyy
+          $ town-crier --sources=http://www.vg.no/rss/feed/,http://www.aftenposten.no/rss/
+          $ town-crier --reset
+    `);
+
+    if (!cli.flags.help && (!config.build || cli.flags.reset)) {
         inquiries.initialize(cb);
 
         return;
     }
 
-    if (argv.help) {
-        meow(`
-            Usage
-              $ town-crier <input>
-
-            Options
-              --help           Help
-              --interval       Interval in seconds
-              --dateFormat     How you'd like your date format
-              --sources        Comma separated list of rss sources you want to append current list
-              --reset          Reset the config file
-
-            Examples
-              $ town-crier --interval=60
-              $ town-crier --dateFormat=mm-dd-yyyy
-              $ town-crier --sources=http://www.vg.no/rss/feed/,http://www.aftenposten.no/rss/
-              $ town-crier --reset
-        `);
-
+    if (cli.flags.help) {
         process.exit(1);
     }
 
     // TODO: create a RSS XML feed validator
-    if (argv.sources) {
-        Array.prototype.push.apply(config.sources, argv.sources.split(','));
+    if (cli.flags.sources) {
+        Array.prototype.push.apply(config.sources, cli.flags.sources.split(','));
     }
 
-    if (argv.interval) {
-        config.interval = argv.interval * 1000;
+    if (cli.flags.interval) {
+        config.interval = cli.flags.interval * 1000;
     }
 
-    if (argv.dateFormat) {
-        config.dateFormat = argv.dateFormat;
+    if (cli.flags.dateFormat) {
+        config.dateFormat = cli.flags.dateFormat;
     }
 
     cb();
